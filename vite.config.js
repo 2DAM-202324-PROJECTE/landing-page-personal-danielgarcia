e84dff19fa13e193c28ab16c11e0dc63a8ca5e5a
Daniel Garcia <dgarcia@iesebre.com>
2023-10-18 21:02:14 +0200

    Update vite.config.js afegir punt d'entrada preus/index.html

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
 export default {
     // config options
     base: '/landing-page-personal-danielgarcia/'
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          preus: resolve(__dirname, 'preus/index.html'),
+        },
+      },
+    },
 }
