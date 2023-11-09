ecfe0d1e017d14a173c519271d7a220c77229f80
Daniel Garcia <dgarcia@iesebre.com>
2023-11-09 15:24:46 +0100

    Afegir configuracons per fer servir particles.js sortejant problemes per no ser un mòdul JS (ESM) i haver de tractar-lo com recurs static

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 
 export default {
+    resolve: {
+        alias: [
+            //alias per referenciar en html una carpeta particlesjs quan en realitat està en node_modules/paticles.js. Permet mantenir el mateix path en dev i prod.
+            {find: /^particlesjs/, replacement: 'node_modules/particles.js'}
+        ]
+    },
     // config options
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {   //copia un js que no es pot funcionar com Module a una carpeta destí que s'ubicarà en dist
+                    src: 'node_modules/particles.js/particles.js',
+                    dest: 'particlesjs'
+                }
+            ]
+        })
+    ],
     base: '/landing-page-personal-danielgarcia/',
     build: {
       rollupOptions: {
