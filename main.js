d83f8d8a236dc8561f282759598d0983ab71f4c3
Daniel Garcia <dgarcia@iesebre.com>
2023-11-09 15:25:12 +0100

    Implementar animació geometrica amb particles.js

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
